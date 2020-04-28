272ec0620013fb4b7b15be38bffa0f20741ef402
whoji <minli1985@gmail.com>
2020-04-29 06:44:36 +0800

    added the env

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 # Study Material
 * Maxim Lapan 's RL book, Chapter 8
 	* github [link](https://github.com/PacktPublishing/Deep-Reinforcement-Learning-Hands-On/tree/master/Chapter08)
+	* environment: pytorch, gym
---
+++ max-book-chap8/lib/environ.py
@@ -0,0 +1,68 @@
+import gym
+import enum
+import data_io as data
+import numpy as np
+
+DEFAULT_BARS_COUNT = 10
+DEFAULT_COMMISSION_PERC = 0.0
+
+class Actions(enum.Enum):
+	Skip = 0
+	Buy = 1
+	Close = 2  # close means 'Sell All'
+
+
+class StockEnv(gym.Env):
+	"""docstring for StockEnv"""
+	metadata = {'render.modes': ['human']}
+
+    def __init__(self, prices, bars_count=DEFAULT_BARS_COUNT,
+                 commission=DEFAULT_COMMISSION_PERC, reset_on_close=True, state_1d=False,
+                 random_ofs_on_reset=True, reward_on_close=False, volumes=False):
+    	assert isinstance(prices, dict)
+    	self._prices = prices
+    	if state_1d:
+    		self._state = State1D(bars_count, commission, reset_on_close, reward_on_close,volumes)
+    	else:
+    		self._state = State(bars_count, commission, reset_on_close, reward_on_close,volumes)
+
+    	self.action_space = gym.spaces.Discrete(n=len(Actions))
+    	self.obs_space = gym.spaces.Box(Low=-np.inf, high=np.inf, shape=self._state.shape, dtype=np.float32)
+    	self.random_ofs_on_reset = random_ofs_on_reset
+    	self.seed()
+
+    @classmethod
+    def from_dir(cls, data_dir, **kwargs):
+        prices = {file: data.load_relative(file) for file in data.price_files(data_dir)}
+        return StocksEnv(prices, **kwargs)
+
+    def reset(self):
+        # make selection of the instrument and it's offset. Then reset the state
+        self._instrument = self.np_random.choice(list(self._prices.keys()))
+        prices = self._prices[self._instrument]
+        bars = self._state.bars_count
+        if self.random_ofs_on_reset:
+            offset = self.np_random.choice(prices.high.shape[0]-bars*10) + bars
+        else:
+            offset = bars
+        self._state.reset(prices, offset)
+        return self._state.encode()
+
+	def step(self, action_idx):
+        action = Actions(action_idx)
+        reward, done = self._state.step(action)
+        obs = self._state.encode()
+        info = {"instrument": self._instrument, "offset": self._state._offset}
+        return obs, reward, done, info
+
+    def render(self, mode='human', close=False):
+        pass
+
+    def close(self):
+        pass
+
+    def seed(self, seed=None):
+        self.np_random, seed1 = seeding.np_random(seed)
+        seed2 = seeding.hash_seed(seed1 + 1) % 2 ** 31
+        return [seed1, seed2]
+
