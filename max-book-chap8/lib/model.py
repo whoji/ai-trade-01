9707f5f1e8914cbb59f974297910be78e7d2eb93
whoji <minli1985@gmail.com>
2020-04-29 14:24:52 +0800

    add the model. exactly followed the book...

---
+++ max-book-chap8/lib/model.py
@@ -0,0 +1,77 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleFFDQN(nn.Module):
+	"""docstring for SimpleFFDQN"""
+	def __init__(self, obs_len, actions_n):
+		super(SimpleFFDQN, self).__init__()
+
+		# V: Value
+		self.fc_val = nn.Sequeuential(
+			nn.Linear(obs_len, 512),
+			nn.ReLU(),
+			nn.Linear(512, 512),
+			nn.ReLU(),
+			nn.Linear(512, 1),
+		)
+
+		# A: Advantage
+		self.fc_adv = nn.Sequeuential(
+			nn.Linear(obs_len, 512),
+			nn.ReLU(),
+			nn.Linear(512, 512),
+			nn.ReLU(),
+			nn.Linear(512, actions_n),
+		)
+
+	def forward(self, x):
+		val = self.fc_val(x)
+		adv = self.fc_adv(x)
+		return val + adv - adv.mean()
+
+
+class DQNConv1D(nn.Module):
+	"""docstring for SimpleFFDQN"""
+	def __init__(self, shape, actions_n):
+		super(DQNConv1D, self).__init__()
+
+		# V: Value
+		self.conv = nn.Sequeuential(
+			nn.Conv1d(shape[0], 128, 5),
+			nn.ReLU(),
+			nn.Conv1d(128, 128, 5),
+			nn.ReLU(),
+		)
+
+		out_size = self._get_conv_out(shape)
+
+		# V: Value
+		self.fc_val = nn.Sequeuential(
+			nn.Linear(out_size, 512),
+			nn.ReLU(),
+			#nn.Linear(512, 512),
+			#nn.ReLU(),
+			nn.Linear(512, 1),
+		)
+
+		# A: Advantage
+		self.fc_adv = nn.Sequeuential(
+			nn.Linear(obs_len, 512),
+			nn.ReLU(),
+			#nn.Linear(512, 512),
+			#nn.ReLU(),
+			nn.Linear(512, actions_n),
+		)
+
+	def _get_conv_out(self, shape):
+		o = self.conv(torch.zeros(1, *shape))
+		return int(np.prod(o.size()))
+
+	def forward(self, x):
+		conv_out = self.conv(x).view(x.size()[0], -1)
+		val = self.fc_val(conv_out)
+		adv = self.fc_adv(conv_out)
+		return val + adv - adv.mean()		
